dd4b0eec876c0f014778cc596000350da0d25930
1emonislam <emonislamweb@gmail.com>
2022-02-27 23:23:36 +0600

    review add update

--- controllers/reviewControllers.js
+++ controllers/reviewControllers.js
@@ -38,4 +38,40 @@ const giveAReview = async (req, res, next) => {
         next(error)
     }
 }
-module.exports = { giveAReview }
+const reviewUpdate = async (req, res, next) => {
+    const { rating, comment } = req.body;
+    if (Number(rating) >= 6 || Number(rating) === 0) {
+        return res.status(404).json({ error: { "product": "Product review rate! Please enter a minimum of 1 to a maximum of 5" } })
+    }
+    const product = await Product.findOne({ _id: req?.params?.id });
+    try {
+        if (product) {
+            const review = {
+                name: req?.user?.name,
+                rating: Number(rating),
+                comment,
+                user: req?.user?._id,
+                product: req?.params?.id
+            }
+            const reviewUpdate = await ProductReview.updateOne({ product: req.params.id, user: req.user._id }, review, { new: true });
+            if (reviewUpdate) {
+                const finding = await ProductReview.find({ product: req.params.id });
+                product.numReviews = finding?.length;
+                product.rating = finding.reduce((acc, item) => item.rating + acc, 0) / finding?.length;
+                await product.save();
+                const resReview = await ProductReview.findOne({ product: req.params.id, user: req.user._id });
+                if (resReview) {
+                    return res.status(200).json({ message: "Review update successfully!", resReview })
+                } else {
+                    return res.status(400).json({ error: { "product": "Review update failed!" } })
+                }
+            }
+        } else {
+            return res.status(404).json({ error: { "product": "Product not founds!" } })
+        }
+    }
+    catch (error) {
+        next(error)
+    }
+}
+module.exports = { giveAReview, reviewUpdate }
