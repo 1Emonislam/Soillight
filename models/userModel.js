443b7a780825e9ca9ec215602435cb46a4be9be2
1emonislam <emonislamweb@gmail.com>
2022-03-03 16:44:52 +0600

    error handler

--- models/userModel.js
+++ models/userModel.js
@@ -90,7 +90,7 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 }
 userSchema.pre('save', async function (next) {
-    if (this.isModified("password")) {
+    if (!this.isModified("password")) {
         return next()
     }
     const salt = await bcrypt.genSalt(10)
