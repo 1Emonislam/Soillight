48411f8419ff7b9679b8621312509b5e3f4b62d0
1emonislam <emonislamweb@gmail.com>
2022-03-03 16:44:07 +0600

    password change

--- models/userModel.js
+++ models/userModel.js
@@ -90,9 +90,11 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 }
 userSchema.pre('save', async function (next) {
-    const hash = await bcrypt.hash(this.password, 10);
-    this.password = hash;
-    next()
+    if (this.isModified("password")) {
+        return next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 const User = mongoose.model('User', userSchema);
 module.exports = User;
