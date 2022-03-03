25ab31271f9a67959129613f6a786dcb236a0407
1emonislam <emonislamweb@gmail.com>
2022-03-03 12:25:02 +0600

    password change

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 // const dbConnect = async (req, res) => {
 //     try {
 //         await mongoose.connect('mongodb://localhost:27017/soilight');
