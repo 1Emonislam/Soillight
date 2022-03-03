0ad5fc394afc57769074ea744234eb436c6aa148
1emonislam <emonislamweb@gmail.com>
2022-03-03 17:13:01 +0600

    added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { registrationSeller, registrationBuyer, registrationRider, login, profileUpdate, singleUser, userApproved, userRejected, changePassword } = require('../controllers/userControllers');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
-router.route('/users/:id').get(protect, singleUser)
 router.route('/users').put(protect, profileUpdate)
 router.route('/users/login').post(login);
+router.route('/users/:id').get(protect, singleUser)              
 router.route('/users/buyer').post(registrationBuyer);
 router.route('/users/seller').post(registrationSeller);
 router.route('/users/change-password').put(protect,changePassword);
