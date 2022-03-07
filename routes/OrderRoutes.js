4185cb71ce70ea5313145f48d4869f8a1e2a76cc
1emonislam <emonislamweb@gmail.com>
2022-03-07 11:16:49 +0600

    all ok

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -2,5 +2,5 @@ const express = require('express');
 const { orderAdd } = require('../controllers/OrderControllers');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
-router.route("products/order").post(protect,orderAdd)
+router.route("/products/order").post(protect,orderAdd)
 module.exports = router;
