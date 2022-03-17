b146445c25f9d6871eeecfec22d15b2b872d2c8f
1emonislam <emonislamweb@gmail.com>
2022-03-17 23:11:46 +0600

    dub rm

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { orderAdd, orderGet, orderSearch, adminSeenOrdersSearch, singleOrder, orderCompeleteToBlanceAdd, orderCancelToBalanceSub, orderStatusUpdate, orderStatusUpdatedMyHistory, allStatusOrder } = require("../controllers/OrderControllers");
+const { orderAdd, orderGet, orderSearch, adminSeenOrdersSearch, singleOrder, orderStatusUpdate, orderStatusUpdatedMyHistory, allStatusOrder } = require("../controllers/OrderControllers");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 router.route("/products/order").post(protect, orderAdd);
@@ -9,7 +9,5 @@ router.route("/products/orders/query").get(protect, allStatusOrder);
 router.route("/products/orders/history").get(protect, orderStatusUpdatedMyHistory);
 router.route("/products/orders/searching").get(protect, adminSeenOrdersSearch);
 router.route("/products/order/status/:id").put(protect, orderStatusUpdate)
-router.route("/products/orders/cancelled/:id").put(protect, orderCancelToBalanceSub);
-router.route("/products/orders/completed/:id").put(protect, orderCompeleteToBlanceAdd);
 router.route("/products/orders/singleOrder/:id").get(protect, singleOrder);
 module.exports = router;
