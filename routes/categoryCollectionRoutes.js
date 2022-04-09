1bfc430a2316aa5f21ca04b5f8c8e9fa2e0572ff
1emonislam <emonislamweb@gmail.com>
2022-04-09 20:59:00 +0600

    category subgategory new added and populated

--- routes/categoryCollectionRoutes.js
+++ routes/categoryCollectionRoutes.js
@@ -4,6 +4,6 @@ const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 router.route("/category-collection-get").get(categoryCollectionGet)
 router.route("/category-collection-add").post(protect, categoryCollectionAdd)
-router.route("/category-collection-update/:id").put(protect, categoryCollectionUpdate)
-router.route("/category-collection-removed/:id").delete(protect, categoryCollectionRemove)
+router.route("/category-collection-update").put(protect, categoryCollectionUpdate)
+router.route("/category-collection-removed").delete(protect, categoryCollectionRemove)
 module.exports = router;
