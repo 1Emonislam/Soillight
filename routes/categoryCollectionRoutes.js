d97f1c7955572757820018d41f8b039b19519a2f
1emonislam <emonislamweb@gmail.com>
2022-04-10 15:40:33 +0600

    category get id by

--- routes/categoryCollectionRoutes.js
+++ routes/categoryCollectionRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { categoryCollectionAdd, categoryCollectionRemove, categoryCollectionUpdate, categoryCollectionGet } = require('../controllers/categoryCollectionControllers');
+const { categoryCollectionAdd, categoryCollectionRemove, categoryCollectionUpdate, categoryCollectionGet, subCategoryAndCategoryCollectionGet } = require('../controllers/categoryCollectionControllers');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 router.route("/category-collection-get").get(categoryCollectionGet)
 router.route("/category-collection-add").post(protect, categoryCollectionAdd)
 router.route("/category-collection-update").put(protect, categoryCollectionUpdate)
 router.route("/category-collection-removed").delete(protect, categoryCollectionRemove)
-router.route("/sub-category-and-category-collection-get").get(protect, categoryCollectionRemove)
+router.route("/sub-category-and-category-collection-get").get(subCategoryAndCategoryCollectionGet)
 module.exports = router;
