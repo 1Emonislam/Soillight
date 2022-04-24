18f58525df5bff29af45dfd0415ef909a6d70b1a
1emonislam <emonislamweb@gmail.com>
2022-04-24 16:15:01 +0600

    added and update category

--- controllers/categoryCollectionControllers.js
+++ controllers/categoryCollectionControllers.js
@@ -22,7 +22,7 @@ const subCategoryCreate = async (req, res, next) => {
         if (!create) {
             return res.status(400).json({ error: { subCategory: 'Sub Category creation failed!' } })
         }
-        const resData = await SubCategory.findOne({_id:create?._id}).populate("category","_id category")
+        const resData = await SubCategory.findOne({ _id: create?._id }).populate("category", "_id category")
         return res.status(200).json({ message: "Sub Category Creation Successfully", data: resData })
     }
     catch (error) {
@@ -38,7 +38,7 @@ const insideSubCategoryCreate = async (req, res, next) => {
         if (!create) {
             return res.status(400).json({ error: { insideSubCategory: 'Inside Sub Category creation failed!' } })
         }
-        const resData = await InsideSubCategory.findOne({_id:create?._id}).populate("category","_id category").populate("subCategory","_id subCategory")
+        const resData = await InsideSubCategory.findOne({ _id: create?._id }).populate("category", "_id category").populate("subCategory", "_id subCategory")
         return res.status(200).json({ message: "Inside Sub Category Creation Successfully", data: resData })
     }
     catch (error) {
@@ -55,7 +55,7 @@ const insidePackTypeCreate = async (req, res, next) => {
         if (!create) {
             return res.status(400).json({ error: { packType: 'packType creation failed!' } })
         }
-        const resData = await InsidePackType.findOne({_id:create?._id}).populate("category","_id category").populate("subCategory","_id subCategory").populate("insideSubCategory","_id insideSubCategory")
+        const resData = await InsidePackType.findOne({ _id: create?._id }).populate("category", "_id category").populate("subCategory", "_id subCategory").populate("insideSubCategory", "_id insideSubCategory")
         return res.status(200).json({ message: "pack type Creation Successfully", data: resData })
 
     }
@@ -74,7 +74,89 @@ const insideSurvingSizeCreate = async (req, res, next) => {
         if (!create) {
             return res.status(400).json({ error: { servingSize: 'Serving Size creation failed!' } })
         }
-        const resData = await InsideServingSize.findOne({_id:create?._id}).populate("category","_id category").populate("subCategory","_id subCategory").populate("insideSubCategory","_id insideSubCategory").populate("packType","_id packType")
+        const resData = await InsideServingSize.findOne({ _id: create?._id }).populate("category", "_id category").populate("subCategory", "_id subCategory").populate("insideSubCategory", "_id insideSubCategory").populate("packType", "_id packType")
+        return res.status(200).json({ message: "Serving Size Creation Successfully", data: resData })
+    }
+    catch (error) {
+        next(error)
+    }
+}
+
+const categoryUpdate = async (req, res, next) => {
+    try {
+        const category = req.body?.category;
+        const update = await Category.findOneAndUpdate({ _id: req?.params?.id }, {
+            category: category
+        }, { new: true });
+        if (!update) {
+            return res.status(400).json({ error: { category: 'Category updated failed!' } })
+        }
+        return res.status(200).json({ message: "Category updated sucessfully", data: update })
+
+    }
+    catch (error) {
+        next(error)
+    }
+}
+const subCategoryUpdate = async (req, res, next) => {
+    try {
+        const subCategory = req.body?.subCategory;
+        const update = await SubCategory.findOneAndUpdate({ _id: req?.params?.id }, {
+            subCategory: subCategory
+        }, { new: true });
+        if (!update) {
+            return res.status(400).json({ error: { subCategory: 'Sub Category updated failed!' } })
+        }
+        const resData = await SubCategory.findOne({ _id: update?._id }).populate("category", "_id category")
+        return res.status(200).json({ message: "Sub category updated successfully", data: resData })
+    }
+    catch (error) {
+        next(error)
+    }
+}
+const insideSubCategoryUpdate = async (req, res, next) => {
+    try {
+        const insideSubCategory = req.body?.insideSubCategory;
+        const update = await InsideSubCategory.findOneAndUpdate({ _id: req.params.id }, {
+            insideSubCategory
+        }, { new: true });
+        if (!update) {
+            return res.status(400).json({ error: { insideSubCategory: 'Inside Sub Category updated failed!' } })
+        }
+        const resData = await InsideSubCategory.findOne({ _id: update?._id }).populate("category", "_id category").populate("subCategory", "_id subCategory")
+        return res.status(200).json({ message: "Inside Sub Category update Successfully", data: resData })
+    }
+    catch (error) {
+        next(error)
+    }
+}
+const insidePackTypeUpdate = async (req, res, next) => {
+    try {
+        const packType = req.body?.packType;
+        const update = await InsidePackType.findOneAndUpdate({ _id: req.params.id }, {
+            packType
+        }, { new: true });
+        if (!update) {
+            return res.status(400).json({ error: { packType: 'Pack Type updated failed!' } })
+        }
+        const resData = await InsidePackType.findOne({ _id: update?._id }).populate("category", "_id category").populate("subCategory", "_id subCategory").populate("insideSubCategory", "_id insideSubCategory")
+        return res.status(200).json({ message: "Pack Updated Successfully", data: resData })
+
+    }
+    catch (error) {
+        next(error)
+    }
+}
+const insideSurvingSizeUpdate = async (req, res, next) => {
+    try {
+        const servingSize = req.body?.servingSize;
+        const update = await InsideServingSize.findOneAndUpdate({ _id: req.params.id }, {
+            servingSize
+        }, { new: true });
+        if (!update) {
+            return res.status(400).json({ error: { servingSize: 'Serving Size creation failed!' } })
+        }
+        const resData = await InsideServingSize.findOne({ _id: update?._id }).populate("category", "_id category").populate("subCategory", "_id subCategory").populate("insideSubCategory", "_id insideSubCategory").populate("packType", "_id packType")
         return res.status(200).json({ message: "Serving Size Creation Successfully", data: resData })
     }
     catch (error) {
@@ -87,5 +169,10 @@ module.exports = {
     subCategoryCreate,
     insideSubCategoryCreate,
     insidePackTypeCreate,
-    insideSurvingSizeCreate
+    insideSurvingSizeCreate,
+    categoryUpdate,
+    subCategoryUpdate,
+    insideSubCategoryUpdate,
+    insidePackTypeUpdate,
+    insideSurvingSizeUpdate,
 }
--- routes/categoryCollectionRoutes.js
+++ routes/categoryCollectionRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { categoryCreate, subCategoryCreate, insideSubCategoryCreate, insidePackTypeCreate, insideSurvingSizeCreate } = require('../controllers/categoryCollectionControllers');
+const { categoryCreate, subCategoryCreate, insideSubCategoryCreate, insidePackTypeCreate, insideSurvingSizeCreate, categoryUpdate, subCategoryUpdate, insidePackTypeUpdate, insideSubCategoryUpdate, insideSurvingSizeUpdate } = require('../controllers/categoryCollectionControllers');
 const { protect } = require('../middlewares/authMiddleware');
 router.post('/category', protect, categoryCreate);
 router.post('/sub/category', protect, subCategoryCreate);
 router.post('/inside/pack/type', protect, insidePackTypeCreate);
 router.post('/inside/sub/category', protect, insideSubCategoryCreate);
 router.post('/inside/serving/size', protect, insideSurvingSizeCreate);
+router.put('/category/:id', protect, categoryUpdate);
+router.put('/sub/category/:id', protect, subCategoryUpdate);
+router.put('/inside/pack/type/:id', protect, insidePackTypeUpdate);
+router.put('/inside/sub/category/:id', protect, insideSubCategoryUpdate);
+router.put('/inside/serving/size/:id', protect, insideSurvingSizeUpdate);
 module.exports = router;
