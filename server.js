9bf45385096371a5712828f3f98a0e0c219e642c
1emonislam <emonislamweb@gmail.com>
2022-05-23 17:45:22 +0600

    port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ global.io = io;
 // socket server binding
 // const socketServer = require("./socket/server");
 // socketServer();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 7000;
 const { errorLog, errorHandlerNotify } = require("express-error-handle");
 const dbConnect = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
